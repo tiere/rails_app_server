62b1218d5e5345ed7f44462af56142c3b0d70315
tiere <tommi.tiere@gmail.com>
2015-12-13 18:49:30 +0200

    Add more attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,19 @@
 # Application
-default['rails_app_server']['database']['name'] = nil
-default['rails_app_server']['name']             = nil
-default['rails_app_server']['repo']             = nil
-default['rails_app_server']['user']             = nil
+default['rails_app_server']['database']['name']     = nil
+default['rails_app_server']['database']['password'] = nil
+default['rails_app_server']['name']                 = nil
+default['rails_app_server']['repo']                 = nil
+default['rails_app_server']['user']                 = nil
+default['rails_app_server']['deploy_public_key']    = nil
+default['rails_app_server']['deploy_private_key']   = nil
+default['rails_app_server']['secret_key_base']      = nil
+default['rails_app_server']['attachment_hash']      = nil
+default['rails_app_server']['ssl_key']              = nil
+default['rails_app_server']['ssl_cert']             = nil
+default['rails_app_server']['devise_secret_key']    = nil
+default['rails_app_server']['mail_domain']          = nil
+default['rails_app_server']['mail_user_name']       = nil
+default['rails_app_server']['mail_password']        = nil
 
 # Rbenv and Ruby
 default['rbenv']['group_users'] = [node['rails_app_server']['user']]
